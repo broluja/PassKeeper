6532379eb3555d79c5132b762d2a17ab131478be
Branko <olujic.branko@gmail.com>
2022-07-16 18:37:52 +0200

    removing token func from utils.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-from dotenv import load_dotenv
-
-load_dotenv()
-
-ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", 60))
-ALGORITHM = "HS256"
-SECRET_KEY = os.getenv('SECRET_KEY')
--- utils.py
+++ utils.py
@@ -1,28 +1,13 @@
-from datetime import datetime, timedelta
-from typing import Union, Any
-from jose import jwt
 from passlib.context import CryptContext
-
-from config import ACCESS_TOKEN_EXPIRE_MINUTES, ALGORITHM, SECRET_KEY
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def get_password_hash(password: str) -> str:
+    """ Hashing a password. """
     return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    """ Verifying equality between hashed password and normal text. """
     return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(subject: Union[str, Any], email: str, expires_delta: timedelta = None) -> str:
-    if expires_delta:
-        expire = datetime.utcnow() + expires_delta
-    else:
-        expire = datetime.utcnow() + timedelta(
-            minutes=ACCESS_TOKEN_EXPIRE_MINUTES
-        )
-    to_encode = {"exp": expire, "sub": str(subject), "email": str(email)}
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
